7b318531cae76699c89ea1a4f605c9467ed05136
Carlos <carlos_alberto_lira@hotmail.com>
2021-05-13 19:46:08 -0500

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Vue Js</h1>
+  <Car />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Car from "./components/Car";
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    Car,
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
---
+++ src/components/Car.vue
@@ -0,0 +1,31 @@
+<template>
+  <h2>Coche</h2>
+  <p>Marca: {{ brand }}</p>
+  <p>Modelo: {{ model }}</p>
+  <p>Potencias</p>
+  <ul>
+    <template v-for="(power, index) in powers" :key="index">
+      <li v-if="power < 280">Urbana: {{ power }}</li>
+      <li v-else-if="power === 280">Hibrido: {{ power }}</li>
+      <li v-else>Carrera: {{ power }}</li>
+    </template>
+  </ul>
+</template>
+
+<script>
+export default {
+  setup() {
+    const brand = "Audi";
+    const model = "A4";
+    const powers = [60, 80, 120, 160, 200, 280, 300, 390, 540, 500];
+
+    return {
+      brand,
+      model,
+      powers,
+    };
+  },
+};
+</script>
+
+<style></style>
